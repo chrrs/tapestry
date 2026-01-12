3f89de8545f79b071486c105d634edea1ca7d497
chrrs <git@chrr.me>
2026-01-12 21:59:27 +0100

    feat: add a task to collect all the merged jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,10 @@
 group = "me.chrr.tapestry"
 version = property("tapestry.version") as String
+
+tasks.register<Copy>("collectJars") {
+    group = "build"
+    description = "Collect all jars from subprojects and put them in a single directory."
+
+    from(subprojects.mapNotNull { it.tasks.findByName("mergedJar") })
+    into(layout.buildDirectory.dir("libs"))
+}
