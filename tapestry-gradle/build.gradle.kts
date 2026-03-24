8dc154ae1a27167187c1f470528dc4a6c909c048
chrrs <git@chrr.me>
2026-03-24 19:29:07 +0100

    chore(gradle): update dependencies

--- tapestry-gradle/build.gradle.kts
+++ tapestry-gradle/build.gradle.kts
@@ -30,8 +30,8 @@ dependencies {
     implementation("io.hotmoka:toml4j:0.7.3")
 
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
-    implementation(plugin("net.fabricmc.fabric-loom", "1.15.2"))
-    implementation(plugin("net.neoforged.moddev", "2.0.139"))
+    implementation(plugin("net.fabricmc.fabric-loom", "1.15.5"))
+    implementation(plugin("net.neoforged.moddev", "2.0.141"))
     implementation(plugin("me.modmuss50.mod-publish-plugin", "1.1.0"))
 }
 
